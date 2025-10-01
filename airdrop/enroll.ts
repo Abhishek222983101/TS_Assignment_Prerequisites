6f9b19f2cddffb86bf79763b59d7ec8ce8ad7423
Abhishek Tiwari <141759667+Abhishek222983101@users.noreply.github.com>
2025-10-02 00:00:55 +0530

    enroll.ts

--- airdrop/enroll.ts
+++ airdrop/enroll.ts
@@ -1,39 +1,48 @@
-import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { Connection, Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
 import { Program, Wallet, AnchorProvider } from "@coral-xyz/anchor";
-import { IDL, Turbin3Prereq } from "./programs/Turbin3_prereq"; 
-import wallet from "./Turbin3-wallet.json";
+import { IDL, Turbin3Prereq } from "./programs/Turbin3_prereq";
+import wallet from "./dev-wallet.json";
+import prompt from "prompt-sync";
 
 
-// We're going to import our keypair from the wallet file 
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
-
-// Create a devnet connection 
 const connection = new Connection("https://api.devnet.solana.com");
 
-// Github account 
-const github = Buffer.from("brianobot", "utf8"); 
 
-// Create our anchor provider 
-const provider = new AnchorProvider(connection, new Wallet(keypair), { commitment: "confirmed"});
+const githubHandle = prompt()("Enter your GitHub handle: ");
+const github = Buffer.from(githubHandle, "utf8");
+const programId = new PublicKey(IDL.address);
 
-// Create our program 
-const program : Program<Turbin3Prereq> = new Program(IDL, provider);
+const provider = new AnchorProvider(connection, new Wallet(keypair), {
+  commitment: "confirmed",
+});
 
+const program = new Program(IDL as Turbin3Prereq, programId, provider);
 
-// Execute our enrollment transaction 
-(async () => { 
-    try { 
-        const txhash = await program.methods 
-            .complete(github) 
-            .accounts({ 
-            signer: keypair.publicKey, 
-        }) 
-        .signers([keypair ])
-        .rpc(); 
+(async () => {
+  try {
+    // Derive PDA: seeds ['prereqs', signer public key]
+    const [prereqPda] = await PublicKey.findProgramAddress(
+      [Buffer.from("prereqs"), keypair.publicKey.toBuffer()],
+      programId
+    );
 
-        console.log(`✅ Success! Check out your TX here: https://explorer.solana.com/tx/${txhash}?cluster=devnet`); 
-    } catch(e) {
-        console.error(`❌ Oops, something went wrong: ${e}`) 
-    } 
-})(); 
-    
+    // Call program's complete method with args and accounts
+    const txhash = await program.methods
+      .complete([...github])
+      .accounts({
+        signer: keypair.publicKey,
+        prereq: prereqPda,
+        system_program: SystemProgram.programId,
+      })
+      .signers([keypair])
+      .rpc();
+
+    console.log(
+      `✅ Enrollment success! TX: https://explorer.solana.com/tx/${txhash}?cluster=devnet`
+    );
+    console.log(`PDA account: ${prereqPda.toBase58()}`);
+  } catch (e) {
+    console.error(`❌ Enrollment error: ${e}`);
+  }
+})();
